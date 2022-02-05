7aede492cba756a3d72251c40e5a481434d3e390
Manan Jain <manan.nitdl@gmail.com>
2022-02-05 23:20:38 +0530

    added maps

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const socketio = require("socket.io");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+//dashboard --> main.html file
 
 
 const app = express();
@@ -13,6 +18,37 @@ const io = socketio(server);
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb+srv://mananjain:123@cluster0.mdrmi.mongodb.net/myFirstDatabase");
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    contact1: String,
+    contact2: String,
+    contact3: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+let userName;
 
 app.get("/",function(req,res){
     res.render("homePage");
@@ -26,6 +62,145 @@ app.get("/register",function(req,res){
     res.render("loginSignup");
 });
 
+// app.get("/dashboard",function(req,res){
+//     if(req.isAuthenticated()){
+//         res.render("dashboard");
+//     }
+//     else{
+//         res.redirect("/login");
+//     }
+// });
+
+// ROUTE TO UPDATE PROFILE//////
+app.get("/settings",function(req,res){
+    res.render('settings',{userName: userName});
+});
+
+
+app.get("/logout",function(req,res){
+    req.logOut();
+    res.redirect("/");
+});
+
+io.on('connection', socket => {
+	socket.on('sendLoc', (myLatitude, myLongitude) => {
+        let phone1;
+        let phone2;
+        let phone3;
+        User.find({username: userName},function(err,foundUser){
+            phone1 = foundUser.contact1;
+            phone2 = foundUser.contact2;
+            phone3 = foundUser.contact3;
+            client.messages
+					.create({
+						body: `I need immediate help at ${myLatitude}, ${myLongitude}`,
+						from: '+18593747178',
+						to: `${phone1}`
+					})
+					.then(message => console.log(message.sid));
+				client.messages
+					.create({
+						body: `I need immediate help at ${myLatitude}, ${myLongitude}`,
+						from: '+18593747178',
+						to: `${phone2}`
+					})
+					.then(message => console.log(message.sid));
+				client.messages
+					.create({
+						body: `I need immediate help at ${myLatitude}, ${myLongitude}`,
+						from: '+18593747178',
+						to: `${phone3}`
+					})
+				.then(message => console.log(message.sid));
+        });
+		console.log(myLatitude);
+		console.log(myLongitude);
+	});
+})
+
+app.post("/settings",function(req,res){
+    User.findOne({'username':req.body.username},function(err,data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            User.findOneAndDelete({'username': req.body.username},function(err,d){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log(d);
+                }
+            });
+           User.register({
+               username: req.body.username,
+               email: req.body.email,
+               contact1: req.body.phone1,
+               contact2: req.body.phone2,
+               contact3: req.body.phone3
+           },req.body.password,function(err,user){
+               if(err){
+                   console.log(err);
+               }
+               else{
+                   console.log(user);
+                   req.logOut();
+                   res.redirect("/login");
+               }
+           });
+        };
+    });
+});
+
+app.post("/login",function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+            res.redirect("/login");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                userName = user.username;
+                res.redirect("/dashboard");
+            });
+        }
+    });
+})
+
+app.post("/register",function(req,res){
+    User.register({
+        username: req.body.username,
+        email: req.body.email,
+        contact1: req.body.phone1,
+        contact2: req.body.phone2,
+        contact3: req.body.phone3
+    }, req.body.password,function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                userName = user.username;
+                res.redirect("/dashboard");
+            });
+        }
+    });
+})
+
+app.get("/dashboard", function (req, res) {
+        if(req.isAuthenticated()){
+	        res.sendFile(__dirname + '/dashboard.html');
+        }
+        else{
+            res.render("/login");
+        }
+});
 
 let port = process.env.PORT;
 if (port == null || port == "") {
